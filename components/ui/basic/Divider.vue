5fb393d5a3165e0a6a865897d05d308b9a3a2f30
Nitesh Kumar Niranjan <niteshkumarniranjan@gmail.com>
2019-10-20 01:46:50 +0530

    Added Divider

---
+++ components/ui/basic/Divider.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex items-center">
+    <span v-if="text" class="pr-3 text-sm uppercase font-semibold text-indigo-500">{{ text }}</span>
+    <span class="flex-1 bg-gray-400" style="height: 2px"></span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: String
+  }
+}
+</script>
