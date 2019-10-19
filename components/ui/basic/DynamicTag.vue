96570352d8b26c5d4844204c0e29b71d6ad68f78
Nitesh Kumar Niranjan <niteshkumarniranjan@gmail.com>
2019-10-20 01:46:35 +0530

    Added Dynamic Tag

---
+++ components/ui/basic/DynamicTag.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  props: {
+    tag: {
+      type: String,
+      required: true
+    },
+    on: {
+      type: Object,
+      default() {
+        return {}
+      }
+    },
+    value: {
+      type: [String, Number]
+    }
+  },
+  render(createElement) {
+    return createElement(
+      this.tag,
+      {
+        on: this.on,
+        domProps: {
+          value: this.value
+        },
+        ref: 'element'
+      },
+      this.$slots.default
+    )
+  },
+  methods: {
+    blur() {
+      this.$refs.element.blur()
+    },
+    focus() {
+      this.$refs.element.focus()
+    }
+  }
+}
+</script>
