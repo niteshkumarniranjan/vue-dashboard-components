8db72497d298940c4e866796d5b4d184a95b5c92
Nitesh Kumar Niranjan <niteshkumarniranjan@gmail.com>
2019-10-20 01:48:15 +0530

    Fixed Divider

--- components/ui/basic/Divider.vue
+++ components/ui/basic/Divider.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="flex items-center">
-    <span v-if="text" class="pr-3 text-sm uppercase font-semibold text-indigo-500">{{ text }}</span>
+    <span
+      v-if="text"
+      class="pr-3 text-sm uppercase font-semibold text-indigo-500 tracking-wide"
+    >{{ text }}</span>
     <span class="flex-1 bg-gray-400" style="height: 2px"></span>
   </div>
 </template>
